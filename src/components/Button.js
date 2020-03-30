48747499c43b0f65bb26a8fc745a74f25bf47532
Sindre Bakke Øyen <sindre.bakke.oyen@gmail.com>
2020-03-30 18:51:36 +0200

    Initial styling of primary and secondary button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,31 +1,36 @@
-import styled, { css } from 'styled-components';
-import '../index.css';
+import styled, { css } from "styled-components";
+import "../index.css";
 
 const Button = styled.button`
   margin: 10px 0;
-  padding: 0.5em 1.5em;
+  padding: 8px 24px;
 
   font-weight: bold;
-  font-size: 16px;
+  font-size: 1rem;
   color: rgb(0, 0, 0, 0.5);
 
   background: transparent;
   border: 2px transparent;
-  border-radius: 1px;
+  border-radius: 5px;
 
   cursor: pointer;
 
-  ${props => props.primary && css`
-    color: #000;
-    background: #ffd900;
-    border: 2px #ffd900 solid;
-  `}
-  ${props => props.secondary && css`
-    border: 2px #212121 solid;
-    color: rgb(0, 0, 0, 0.7);
-  `}
+  ${props =>
+    props.primary &&
+    css`
+      color: ${props => (props.yellow ? "var(--gray-90)" : "var(--gray-00)")};
+      background: ${props =>
+        props.yellow ? "var(--primary)" : "var(--gray-90)"};
+      border: 2px
+        ${props => (props.yellow ? "var(--primary)" : "var(--gray-90)")} solid;
+    `}
+  ${props =>
+    props.secondary &&
+    css`
+      color: ${props => (props.yellow ? "var(--primary)" : "var(--gray-90)")};
+      border: 2px
+        ${props => (props.yellow ? "var(--primary)" : "var(--gray-90)")} solid;
+    `}
 `;
 
-export {
-  Button
-};
+export { Button };
