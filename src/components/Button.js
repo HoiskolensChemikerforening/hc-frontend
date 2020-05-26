382e40011a411a7518a01742a29a443df4c9aa92
Anja From <anjarfrom@gmail.com>
2020-05-26 15:50:04 +0200

    Remove neccessary keyword for primary button and set default to light theme. Add comments

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,87 +2,105 @@ import styled, { css } from "styled-components";
 import "../index.css";
 import React from "react";
 
+// Usage:
+// <Button buttonType="primary" dark>Text inside button</Button
+// Keyword dark is omitted for a standard yellow button
 const Button = props => {
+  // Check buttonType="keyword"
   switch (props.buttonType) {
-    case "primary":
+    case "secondary": // buttonType="secondary" was passed in
       return (
-        <PrimaryButton yellow={props.yellow}>{props.children}</PrimaryButton>
+        <SecondaryButton dark={props.dark}>{props.children}</SecondaryButton>
       );
-    case "secondary":
-      return (
-        <SecondaryButton yellow={props.yellow}>
-          {props.children}
-        </SecondaryButton>
-      );
-    case "accept":
+    case "accept": // buttonType="accept" was passed in
       return <AcceptButton>{props.children}</AcceptButton>;
-    case "error":
+    case "error": // buttonType="error" was passed in
       return <ErrorButton>{props.children}</ErrorButton>;
     default:
-      return <div></div>;
+      // Primary button acts as fallback
+      return <PrimaryButton dark={props.dark}>{props.children}</PrimaryButton>;
   }
 };
 
 const PrimaryButton = styled.button`
+  /* Set spacing around button */
   margin: 10px 0;
   padding: 8px 24px;
 
+  /* Set styling related to the text */
   font-weight: bold;
   font-size: 1rem;
+  text-transform: uppercase;
+  color: ${props => (props.dark ? "var(--gray-00)" : "var(--gray-90)")};
 
-  color: ${props => (props.yellow ? "var(--gray-90)" : "var(--gray-00)")};
-
-  background: ${props => (props.yellow ? "var(--primary)" : "var(--gray-90)")};
-  border: 2px ${props => (props.yellow ? "var(--primary)" : "var(--gray-90)")}
+  /* Set rules related to the button box and border */
+  background: ${props => (props.dark ? "var(--gray-90)" : "var(--primary)")};
+  border: 2px ${props => (props.dark ? "var(--gray-90)" : "var(--primary)")}
     solid;
-  border-radius: 5px;
+  border-radius: 0px;
 
+  /* Set pointer when hover */
   cursor: pointer;
 `;
 
 const SecondaryButton = styled.button`
+  /* Set spacing around button */
   margin: 10px 0;
   padding: 8px 24px;
 
+  /* Set styling related to the text */
   font-weight: bold;
   font-size: 1rem;
-  color: ${props => (props.yellow ? "var(--primary)" : "var(--gray-90)")};
+  text-transform: uppercase;
+  color: ${props => (props.dark ? "var(--gray-90)" : "var(--gray-90)")};
 
+  /* Set rules related to the button box and border */
   background: transparent;
-  border: 2px ${props => (props.yellow ? "var(--primary)" : "var(--gray-90)")}
+  border: 2px ${props => (props.dark ? "var(--gray-90)" : "var(--primary)")}
     solid;
-  border-radius: 5px;
+  border-radius: 0px;
 
+  /* Set pointer when hover */
   cursor: pointer;
 `;
 
 const AcceptButton = styled.button`
+  /* Set spacing around button */
   margin: 10px 0;
   padding: 8px 24px;
 
+  /* Set styling related to the text */
   font-weight: bold;
   font-size: 1rem;
+  text-transform: uppercase;
   color: var(--gray-00);
 
+  /* Set rules related to the button box and border */
   background: var(--accept);
   border: 2px var(--accept) solid;
-  border-radius: 5px;
+  border-radius: 0px;
 
+  /* Set pointer when hover */
   cursor: pointer;
 `;
 
 const ErrorButton = styled.button`
+  /* Set spacing around button */
   margin: 10px 0;
   padding: 8px 24px;
 
+  /* Set styling related to the text */
   font-weight: bold;
   font-size: 1rem;
+  text-transform: uppercase;
   color: var(--gray-00);
 
+  /* Set rules related to the button box and border */
   background: var(--error);
   border: 2px var(--error) solid;
-  border-radius: 5px;
+  border-radius: 0px;
 
+  /* Set pointer when hover */
   cursor: pointer;
 `;
 
