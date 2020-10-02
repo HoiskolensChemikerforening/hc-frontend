9b2f8364e6f7fa6b3003a97e3bf0a3d3617deb40
Helene Boee <helebo@stud.ntnu.no>
2020-10-02 17:04:25 +0200

    Display quiz data.

--- src/ExampleApp.js
+++ src/ExampleApp.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./index.css";
 import { ExampleForm } from "./components/Form";
 import { HomePage } from "./pages/home/HomePage";
+import QuizTermBox from "./pages/home/Quiz";
 
 class ExampleApp extends React.Component {
   render() {
     return (
       <>
-        <HomePage />
+
+        <QuizTermBox />
       </>
     );
   }
--- src/pages/home/Quiz.jsx
+++ src/pages/home/Quiz.jsx
@@ -3,3 +3,30 @@ import "../../index.css";
 import {Col, Row} from "../../components/FlexLayout";
 import {P} from "../../components/Text";
 
+function QuizTermBox() {
+    // Declares var quizTerm and quizScores, and an update function, useState sets initial value to an empty list
+    const [quizTerm, setQuizTerm] = useState([]);
+    const [quizScore, setQuizScore] = useState([]);
+
+    // Runs once HomePage component is loaded
+    useEffect(() => {
+        fetchAllQuizData();
+    }, []);
+
+    // Gets all quiz terms and scores from the quiz api url and sets the quizTerm variable
+    async function fetchAllQuizData() {
+        const response = await fetch("http://127.0.0.1:8000/quiz/api/");
+        const jsonData = await response.json();
+        setQuizTerm(jsonData[0]);
+    }
+
+    return(
+        <Col>
+            {quizTerm.map(quizTerm => (
+                <div key={quizTerm.id}>{quizTerm}</div>
+            ))}
+        </Col>
+    );
+}
+
+export default QuizTermBox;
