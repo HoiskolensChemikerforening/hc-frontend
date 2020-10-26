628b01d2229d313b5b006260e4ec08865ae1d114
Helene Boee <helebo@stud.ntnu.no>
2020-10-26 15:20:48 +0100

    Fetch data for quizTerm

--- src/pages/home/Quiz.jsx
+++ src/pages/home/Quiz.jsx
@@ -4,33 +4,29 @@ import {Col, Row} from "../../components/FlexLayout";
 import {P} from "../../components/Text";
 
 function QuizTermBox() {
-    // Declares var quizTerm and quizScores, and an update function, useState sets initial value to an empty list
-    const [quizTerm, setQuizTerm] = useState([]);
-    const [quizScore, setQuizScore] = useState([]);
+  // Declares var quizTerm and quizScores, and an update function, useState sets initial value to an empty list
+  const [quizTerm, setQuizTerm] = useState([]);
+  const [quizScore, setQuizScore] = useState([]);
 
-    // Runs once HomePage component is loaded
-    useEffect(() => {
-        const data = fetchAllQuizData();
-        setQuizTerm(data);
-    }, []);
+  // Runs once HomePage component is loaded
+  useEffect(() => {
+      fetchQuizData();
+  }, []);
+  // Gets all quiz terms and scores from the quiz api url and sets the quizTerm variable
+  async function fetchQuizData() {
+    const response = await fetch("http://127.0.0.1:8000/quiz/api/");
+    const jsonData = await response.json();
+    setQuizTerm(jsonData);
+  }
 
-    // Gets all quiz terms and scores from the quiz api url and sets the quizTerm variable
-    async function fetchAllQuizData() {
-        const response = await fetch("http://127.0.0.1:8000/quiz/api/");
-        const jsonData = await response.json();
-        return jsonData;
 
-    }
-
-    console.log(quizTerm);
-    return(
-        <Col>
-
-{/*            {quizTerm.map(quizTerm => (
-                <div key={quizTerm.id}>{quizTerm}</div>
-            ))}*/}
-        </Col>
-    );
+  return (
+      <div>
+        {quizTerm.map(term => (
+            <div key={term.id}>{term.term}</div>
+        ))}
+      </div>
+  );
 }
 
-export { QuizTermBox };
+export {QuizTermBox};
