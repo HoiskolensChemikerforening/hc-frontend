1800b5da9730b34287dc495a7e5024f7b2d0e520
Helene Boee <helebo@stud.ntnu.no>
2020-11-07 13:50:13 +0100

    Correction fetch url.

--- src/ExampleApp.js
+++ src/ExampleApp.js
@@ -1,17 +1,17 @@
 import React from "react";
 import "./index.css";
-import { ExampleForm } from "./components/Form";
-import { HomePage } from "./pages/home/HomePage";
-import { QuizTermBox  ,  QuizScoreBox } from "./pages/home/Quiz";
+import {ExampleForm} from "./components/Form";
+import {HomePage} from "./pages/home/HomePage";
+import {QuizTermBox, QuizScoreBox} from "./pages/home/Quiz";
 
 class ExampleApp extends React.Component {
   render() {
     return (
-      <>
-        <h1>Hello</h1>
-        <QuizTermBox />
-        <QuizScoreBox />
-      </>
+        <>
+          <h1>Hello</h1>
+          <QuizTermBox/>
+          <QuizScoreBox/>
+        </>
     );
   }
 }
--- src/pages/home/Quiz.jsx
+++ src/pages/home/Quiz.jsx
@@ -1,19 +1,19 @@
 import React, {useState, useEffect} from "react";
 import "../../index.css";
-import {Col, Row} from "../../components/FlexLayout";
-import {P} from "../../components/Text";
 
+// Fetches data from ListAllQuizTerms
 function QuizTermBox() {
-  // Declares var quizTerm and quizScores, and an update function, useState sets initial value to an empty list
+  // Declares var quizTerm and an update function, useState sets initial value to an empty list
   const [quizTerm, setQuizTerm] = useState([]);
 
   // Runs once HomePage component is loaded
   useEffect(() => {
-      fetchQuizData();
+    fetchQuizData();
   }, []);
+
   // Gets all quiz terms (and scores) from the quiz api url and sets the quizTerm variable
   async function fetchQuizData() {
-    const response = await fetch("http://127.0.0.1:8000/quiz/api/");
+    const response = await fetch("http://127.0.0.1:8000/quiz/periode/api/");
     const jsonData = await response.json();
     setQuizTerm(jsonData);
   }
@@ -28,6 +28,7 @@ function QuizTermBox() {
 
 }
 
+// Fetches data from ListAllScores
 function QuizScoreBox() {
   const [quizScore, setQuizScore] = useState([]);
 
@@ -38,12 +39,12 @@ function QuizScoreBox() {
 
   // Gets all quiz score from the quiz api url and sets the quizScore
   async function fetchQuizScore() {
-    const response = await fetch("http://127.0.0.1:8000/quiz/api/");
+    const response = await fetch("http://127.0.0.1:8000/quiz/poeng/api/");
     const jsonData = await response.json();
     setQuizScore(jsonData);
   }
 
-    return (
+  return (
       <div>
         {quizScore.map(score => (
             <div key={score.id}>{score.score}</div>
@@ -53,4 +54,34 @@ function QuizScoreBox() {
 
 }
 
-export {QuizTermBox ,  QuizScoreBox};
+/*
+// Fetches data from QuizTermDetails - data may be retrieved, updated and destroyed
+function QuizTermDetailBox() {
+  const [quizTermDetail, setQuizTermDetail] = useState([]);
+
+  // Runs once HomePage component is loaded
+  useEffect(() => {
+    fetchTermDetail();
+  }, []);
+
+
+  // Gets all quiz score from the quiz api url and sets the quizScore
+  async function fetchTermDetail() {
+    const response = await fetch("http://127.0.0.1:8000/quiz/periode-detaljer/api/1"); // switch 1 with wanted integer/id
+    const jsonData = await response.json();
+    setQuizTermDetail(jsonData);
+  }
+
+    return (
+      <div>
+        {quizTermDetail.map(termDet => (
+            <div key={termDet.id}>{termDet.is_active}</div>
+        ))}
+      </div>
+  );
+
+}
+
+*/
+
+export {QuizTermBox, QuizScoreBox};
