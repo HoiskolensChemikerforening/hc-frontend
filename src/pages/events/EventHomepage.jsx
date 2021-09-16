86421444d656b75bdf799a073c7e17001dcecf92
Eline Lillebø Karlsen <elinelk@stud.ntnu.no>
2021-09-16 15:20:45 +0200

    create eventHomepage

---
+++ src/pages/events/EventHomepage.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from "react";
+import styled, { css } from 'styled-components';
+import { Link, useHistory } from "react-router-dom";
+import { H3, P } from "../../components/Text";
+import { Button } from "../../components/Button.js";
+import parse from "react-html-parser";
+import moment from 'moment';
+import { FiCoffee } from "react-icons/fi";
+import { RiArchiveDrawerLine, RiHandCoinLine, RiDoorOpenFill } from "react-icons/ri";
+import { BiReceipt } from "react-icons/bi";
+import { Container, Col, Row } from "../../components/Layout";
+
+export const EventHomepage = () => {
+  
+  
+    return (
+        <>
+        </>
+    )
+  };
