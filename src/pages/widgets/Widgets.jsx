b7432be42d47b2eaf346ee2dfeeba18aa43d0f21
Eline Lillebø Karlsen <elinelk@stud.ntnu.no>
2021-10-07 14:02:35 +0200

    Fix unused import statements

--- src/pages/widgets/Widgets.jsx
+++ src/pages/widgets/Widgets.jsx
@@ -2,14 +2,8 @@ import React, { useEffect, useState } from "react";
 import styled, { css } from 'styled-components';
 import { Link, useHistory } from "react-router-dom";
 import { H3, P } from "../../components/Text";
-import { Button } from "../../components/Button.js";
-import parse from "react-html-parser";
-import moment from 'moment';
 import { FiCoffee } from "react-icons/fi";
 import { RiArchiveDrawerLine, RiHandCoinLine, RiDoorOpenFill } from "react-icons/ri";
-import { BiReceipt } from "react-icons/bi";
-import { Container, Col, Row } from "../../components/Layout";
-import DateDiff from "date-diff";
 
 export const Widgets = () => {
 
