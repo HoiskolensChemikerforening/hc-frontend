db5610ef81f90f6f6717c2d949ac6d76edac5460
Johannes Kjær <77483678+johannes-kjaer@users.noreply.github.com>
2022-09-18 15:06:37 +0200

    [WIP] Made the 'undergrupper' page, but it still looks awful (#55)
    * [WIP] Made the 'undergrupper' page, but it still looks awful
    
    * Updated the 'undergrupper' page, trying to make pictures display correctly
    
    * Got the images on the subgroups page to display more nicely
    
    * Split Undergrupper.jsx into a Card and a page, and began using arrowfunctions to fetch the relevant committee
    
    * Got the committee cards to work as buttons, and got the title bar to be at the bottom of the cards
    
    * Initiated the committee details page
    
    * Made a simple page to display each committee
    
    * Clean-up of the subgroup card
    
    * Set images to stretch to fit their container, and styled the title a little nicer
    
    * Trying to pass props through useHistory, but as for now it only crashes the page
    
    * Sucessfully passed the 'committee' props through useHistory by the use of use of useLocation, and got the image of the committee to display
    
    * Added a first draft of the individual subgroup pages
    
    * Made the committee details page look okay, by making a card for displaying positions, and placed it dynamically on the page. There is still som styling left.
    
    * Added functionality for displaying the relevant position names, and the users occupying the position. Renamed some stuff for greater consistency. Does still need some styling.

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,15 @@ import "./index.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { HomePage } from "./pages/home/HomePage";
 import { NavBar } from "./components/NavBar";
+import { CommitteePage} from "./pages/subgroups/SubGroups";
 import { H1 } from "./components/Text";
 import { NavBarPhone } from "./components/NavBarPhone";
 import { NewsRouter } from "./pages/news/NewsRouter";
+import { NewsList } from "./pages/news/NewsList";
+import { NewsDetail } from "./pages/news/NewsDetail";
+import { NewsForm} from "./pages/news/NewsForm";
+import { CommitteeDetailsPage } from "./pages/subgroups/CommitteeDetailsPage";
+
 
 class App extends React.Component {
   render() {
@@ -13,13 +19,15 @@ class App extends React.Component {
         <Router>
           <NavBar/>
           <Switch>
-            <Route path="/" exact>        <HomePage/>                     </Route>
-            <Route path="/arrangement">   <H1>BLOT OG SÅNN!!!</H1>    </Route>
-            <Route path="/bedrift">       <H1>Bedrift</H1>            </Route>
-            <Route path="/internt">       <H1>Internt</H1>            </Route>
-            <Route path="/info">          <H1>Info</H1>               </Route>
-            <Route path="/profil">        <H1>Profil</H1>             </Route>
-            <Route path="/nyheter"> <NewsRouter /> </Route>
+            <Route path="/" exact>                  <HomePage/>                 </Route>
+            <Route path="/arrangement">             <H1>BLOT OG SÅNN!!!</H1>    </Route>
+            <Route path="/bedrift">                 <H1>Bedrift</H1>            </Route>
+            <Route path="/internt">                 <H1>Internt</H1>            </Route>
+            <Route path="/info">                    <H1>Info</H1>               </Route>
+            <Route path="/profil">                  <H1>Profil</H1>             </Route>
+            <Route path="/nyheter">                 <NewsRouter/>               </Route>
+            <Route path="/undergrupper/:committee"> <CommitteeDetailsPage/>     </Route>            
+            <Route path="/undergrupper">            <CommitteePage/>            </Route>
           </Switch>
           <NavBarPhone/>
         </Router>
