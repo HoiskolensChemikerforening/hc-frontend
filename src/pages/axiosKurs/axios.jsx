9db6d5cd0e8cd722bb8b8020a822708a1e878944
Eline Lillebø Karlsen <elinelk@stud.ntnu.no>
2023-02-08 17:56:05 +0100

    added file

---
+++ src/pages/axiosKurs/axios.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+export const Axios = () => {
+  return(
+  <>
+    <form>
+        <label>
+        Name:
+        <input type="text" name="name" />
+        </label>
+        <input type="submit" value="Submit" />
+    </form>
+  </>
+)};
