04c7d339d206288b322a75b873c489361259e8e6
Ingrid Sutterud <ingridsutterud@dhcp-10-24-11-66.wlan.ntnu.no>
2023-02-22 17:06:07 +0100

    added functions

--- src/pages/axiosKurs/axios.jsx
+++ src/pages/axiosKurs/axios.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import axios from "axios";
+const postURL = "http://127.0.0.1:8000/web_push/api/";
+const baseURL = "http://127.0.0.1:8000/nyheter/api/"
 
 
 export const Axios = () => {
+  const [post, setPost] = React.useState(null);
+  
+  React.useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      setPost(response.data);
+      console.log(post);
+    });
+  }, []);
+  function createPost() {
+    axios.post(postURL, {
+      date: new Date()
+    })
+  }
   return(
   <>
     <form>
@@ -10,6 +26,7 @@ export const Axios = () => {
         <input type="text" name="name" />
         </label>
         <input type="submit" value="Submit" />
+        <button onClick={createPost()}>Click me!</button>
     </form>
   </>
 )};
