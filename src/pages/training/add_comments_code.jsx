101574dc87f51d8c4b62cade11119c6d3a882c35
Ingrid Sutterud <ingrid.sutterud@gmail.com>
2024-01-06 12:49:32 +0100

    Added some images and styling to the images

--- src/pages/training/add_comments_code.jsx
+++ src/pages/training/add_comments_code.jsx
@@ -2,10 +2,11 @@ import React, { useState, useEffect } from "react";
 import styled from 'styled-components';
 import { PageContainer } from "../../components/Layout";
 import { P, TitleContainer, Title } from '../../components/Text'; 
+import coffeeMug from './coffee_mug.png';
+import coffeeMachine from './coffee_machine.png';
+import waffleHeart from './waffle_heart.png';
 
 export const AddComments = () => {
-
-
     return (
         <PageContainer> 
             <TitleContainer> 
@@ -34,6 +35,11 @@ export const AddComments = () => {
                 HC-kontoret er et naturlig møtepunkt og tilholdssted for oss på Chemie, og vi håper at også dere nye i førsteklasse vil 
                 komme til å føle samme tilhørigheten til kontoret.
                 </P>
+                <Images>
+                    <Photo src={coffeeMug} alt="Coffee Mug" />
+                    <Photo src={waffleHeart} alt="Waffle Heart" />
+                    <Photo src={coffeeMachine} alt="Coffee Machine" />
+                </Images>
             </ContentContainer>
         </PageContainer>
     )
@@ -45,4 +51,17 @@ const ContentContainer = styled.div`
     padding: 20px;
     text-align: center;
     margin: 10px;
+`;
+
+const Images = styled.div`
+    display: flex;
+    flex-direction: row; // Align items in a horizontal row
+    justify-content: center; // Center items horizontally
+    align-items: center; // Center items vertically
+    gap: 50px; // Add some space between the images
+`;
+
+const Photo = styled.img`
+    max-width: 200px;  // Sets the maximum width of the image
+    height: auto;     // Maintains the aspect ratio of the image
 `;
