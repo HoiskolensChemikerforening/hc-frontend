0f4defaae7ec0d4c8adcd0c91baeabcd806b8c2b
Helle Opdal <helleopdal@dhcp-10-22-6-93.wlan.ntnu.no>
2024-03-16 14:45:23 +0100

    boks med skrift inni

--- src/pages/infoPages/teknostart.jsx
+++ src/pages/infoPages/teknostart.jsx
@@ -10,17 +10,23 @@ export const Teknostart = () => {
   
     return (
         <>
-        <TitleContainer><Title>Teknostart</Title></TitleContainer>
-        <InfoText>
-            
-            <P>I løpet av første uken arrangeres Teknostart for alle førsteklassingene på sivilingeniørlinjene ved NTNU. Dette er et 
+        <OuterWrapper>
+          <TitleContainer><Title>Teknostart</Title></TitleContainer>
+            <ContentBox>
+              <InfoText>
+                <P>I løpet av første uken arrangeres Teknostart for alle førsteklassingene på sivilingeniørlinjene ved NTNU. Dette er et 
                 introduksjonskurs hvor man får et innblikk i den studieretningen man har valgt og et minikurs i gruppearbeid. </P> 
             
-            <P>Teknostart er delt inn i flere deler hvor hovedarbeidet består av et prosjektarbeid i grupper. </P>
+                <P>Teknostart er delt inn i flere deler hvor hovedarbeidet består av et prosjektarbeid i grupper. </P>
             
-            <P>Mer informasjon om Teknostart finner du på: <Link> https://www.ntnu.no/studier/teknostart </Link> </P>
+                <P>Mer informasjon om Teknostart finner du på: <Link> https://www.ntnu.no/studier/teknostart </Link> </P>
             
-        </InfoText>
+              </InfoText>
+            </ContentBox>
+          
+
+        </OuterWrapper>
+        
         </>
     )
   };
@@ -28,4 +34,16 @@ export const Teknostart = () => {
 const InfoText = styled.div`
   text-align: center;
   margin: 0 20% 0 20%;
+`;
+
+const OuterWrapper = styled.div`
+  background-color: var(--gray-10);
+  min-height: 100vh;
+`;
+
+const ContentBox = styled.div`
+  background-color: white;
+  border-radius: 10px;
+  margin: 0 2% 0 2%;
+  padding: 2% 5% ;
 `;
